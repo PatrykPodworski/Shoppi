ffb9e2fbede29faefb0099cf8908cf8b9b77db34
Patryk Podworski <patryk3101@gmail.com>
2017-07-02 21:41:02 +0200

    Product controller.

---
+++ Shoppi/Controllers/ProductController.cs
@@ -0,0 +1,21 @@
+﻿using Shoppi.Logic.Abstract;
+using System.Web.Mvc;
+
+namespace Shoppi.Controllers
+{
+    public class ProductController : Controller
+    {
+        private readonly IProductServices _productServices;
+
+        public ProductController(IProductServices productServices)
+        {
+            _productServices = productServices;
+        }
+
+        public ActionResult Index()
+        {
+            var model = _productServices.GetAll();
+            return View(model);
+        }
+    }
+}
