0903855fda07ac325ed3e136c2e53aea666763f9
Patryk Podworski <patryk3101@gmail.com>
2017-07-02 21:40:49 +0200

    Added UnityMVC5 and OWIN.

---
+++ Shoppi/App_Start/UnityConfig.cs
@@ -0,0 +1,27 @@
+using Microsoft.Practices.Unity;
+using Shoppi.Data.Abstract;
+using Shoppi.Data.Repositories;
+using Shoppi.Logic.Abstract;
+using Shoppi.Logic.Implementation;
+using System.Web.Mvc;
+using Unity.Mvc5;
+
+namespace Shoppi
+{
+    public static class UnityConfig
+    {
+        public static void RegisterComponents()
+        {
+            var container = new UnityContainer();
+
+            // register all your components with the container here
+            // it is NOT necessary to register your controllers
+
+            // e.g. container.RegisterType<ITestService, TestService>();
+            container.RegisterType<IProductServices, ProductServices>();
+            container.RegisterType<IProductRepository, ProductRepository>();
+
+            DependencyResolver.SetResolver(new UnityDependencyResolver(container));
+        }
+    }
+}
