1821efe0dc984441a0b17e94f7b7ae4616705c5b
Patryk Podworski <patryk3101@gmail.com>
2017-07-05 17:34:24 +0200

    Implemented home controller with default view.

---
+++ Shoppi/Controllers/HomeController.cs
@@ -0,0 +1,12 @@
+﻿using System.Web.Mvc;
+
+namespace Shoppi.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
