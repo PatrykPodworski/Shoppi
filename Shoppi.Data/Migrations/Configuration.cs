99f490afb4a40d41063c557f982973e8dcdacb8d
Patryk Podworski <patrykpodworski@gmail.com>
2017-07-12 19:50:26 +0200

    Fixed Seed method.

--- Shoppi.Data/Migrations/Configuration.cs
+++ Shoppi.Data/Migrations/Configuration.cs
@@ -3,7 +3,7 @@ namespace Shoppi.Data.Migrations
     using Shoppi.Data.Models;
     using System.Data.Entity.Migrations;
 
-    internal sealed class Configuration : DbMigrationsConfiguration<Shoppi.Data.Models.ShoppiDbContext>
+    internal sealed class Configuration : DbMigrationsConfiguration<ShoppiDbContext>
     {
         public Configuration()
         {
@@ -13,27 +13,31 @@ namespace Shoppi.Data.Migrations
         protected override void Seed(ShoppiDbContext context)
         {
             var sportCategory = new Category("Sport");
-            context.Categories.AddOrUpdate(sportCategory);
 
-            var footballCategory = new Category("Football", sportCategory);
-            context.Categories.AddOrUpdate(footballCategory);
+            var categories = new[]
+            {
+                sportCategory,
+                new Category("Football", sportCategory),
+                new Category("Cycling", sportCategory),
+                new Category("Video games")
+            };
 
-            var cyclingCategory = new Category("Cycling", sportCategory);
-            context.Categories.AddOrUpdate(cyclingCategory);
+            context.Categories.AddOrUpdate(c => c.Name, categories);
 
-            var videoGamesCategory = new Category("Video games");
-            context.Categories.AddOrUpdate(videoGamesCategory);
+            var products = new[]
+            {
+                new Product("Adidas Adizero 5-Star", categories[1].Id, 12),
+                new Product("Nike Inter Milan Prestige Football", categories[1].Id, 36),
+                new Product("Cannondale Synapse Sm 105 5 Disc Road", categories[2].Id, 2),
+                new Product("Altura Hammock Waist Short", categories[2].Id, 58),
+                new Product("Altura Icarus Short Sleeve Tee", categories[2].Id, 92),
+                new Product("Sealskinz Waterproof Bobble Hat", categories[2].Id, 24),
+                new Product("Uncharted: The Nathan Drake Collection", categories[3].Id, 111),
+                new Product("God of War Collection", categories[3].Id, 66),
+                new Product("Bayonetta 2", categories[3].Id, 39)
+            };
 
-            context.Products.AddOrUpdate(new Product("Adidas Adizero 5-Star", footballCategory, 12));
-            context.Products.AddOrUpdate(new Product("Nike Inter Milan Prestige Football", footballCategory, 36));
-            context.Products.AddOrUpdate(new Product("Cannondale Synapse Sm 105 5 Disc Road", cyclingCategory, 2));
-            context.Products.AddOrUpdate(new Product("Altura Hammock Waist Short", cyclingCategory, 58));
-            context.Products.AddOrUpdate(new Product("Altura Icarus Short Sleeve Tee", cyclingCategory, 92));
-            context.Products.AddOrUpdate(new Product("Sealskinz Waterproof Bobble Hat", cyclingCategory, 24));
-            context.Products.AddOrUpdate(new Product("Uncharted: The Nathan Drake Collection", videoGamesCategory, 111));
-            context.Products.AddOrUpdate(new Product("God of War Collection", videoGamesCategory, 66));
-            context.Products.AddOrUpdate(new Product("Bayonetta 2", videoGamesCategory, 39));
-
+            context.Products.AddOrUpdate(p => p.Name, products);
             context.SaveChanges();
         }
     }
