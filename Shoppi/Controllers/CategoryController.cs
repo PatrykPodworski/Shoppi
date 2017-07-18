5aeaed41af2a2d184238a881a2fd1b37c0c5dc8c
Patryk Podworski <patrykpodworski@gmail.com>
2017-07-18 18:04:39 +0200

    Basic category list view.

---
+++ Shoppi/Controllers/CategoryController.cs
@@ -0,0 +1,24 @@
+﻿using Shoppi.Logic.Abstract;
+using Shoppi.Models;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+
+namespace Shoppi.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private ICategoryServices _categoryServices;
+
+        public CategoryController(ICategoryServices categoryServices)
+        {
+            _categoryServices = categoryServices;
+        }
+
+        public async Task<ActionResult> List()
+        {
+            var categories = await _categoryServices.GetAllAsync();
+            var model = new CategoryListViewModel(categories);
+            return View(model);
+        }
+    }
+}
---
+++ Shoppi/Models/CategoryListViewModel.cs
@@ -0,0 +1,20 @@
+﻿using Shoppi.Data.Models;
+using System.Collections.Generic;
+
+namespace Shoppi.Models
+{
+    public class CategoryListViewModel
+    {
+        public List<Category> Categories { get; set; }
+
+        public CategoryListViewModel()
+        {
+            Categories = new List<Category>();
+        }
+
+        public CategoryListViewModel(List<Category> categories)
+        {
+            Categories = categories;
+        }
+    }
+}
