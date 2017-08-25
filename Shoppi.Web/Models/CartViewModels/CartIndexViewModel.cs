b03198255bdcec568cc25837385304013b0340d8
Patryk Podworski <patrykpodworski@gmail.com>
2017-08-25 23:57:28 +0200

    CartIndex view reworked with decrement button script.

--- Shoppi.Web/Models/CartViewModels/CartIndexViewModel.cs
+++ Shoppi.Web/Models/CartViewModels/CartIndexViewModel.cs
@@ -9,6 +9,7 @@ namespace Shoppi.Web.Models.CartViewModels
 
     public class CartLineViewModel
     {
+        public int ProductId { get; set; }
         public string ProductName { get; set; }
         public int Quantity { get; set; }
     }
